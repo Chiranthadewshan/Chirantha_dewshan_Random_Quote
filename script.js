118fd757c8e68263d9f374e31fb092e36f307437
Chiranthadewshan <chiranthadewshan186@gmail.com>
2024-10-08 18:24:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function load(){
+    var x = new XMLHttpRequest();
+
+    x.onreadystatechange = function(){
+        if(x.status == 200 ){
+           var response= JSON.parse(x.responseText);
+            document.getElementById("quote").innerHTML = response.content +" ~ " + response.author;
+        }else if(x.readyState == 4){
+            document.getElementById("quote").innerHTML = "Error! , Please Try Again Later";
+        }
+    }
+    x.onerror = function(){
+        document.getElementById("quote").innerHTML = "Error! , Connection issue";
+    }
+
+    x.open("GET","http://api.quotable.io/random",true);
+    x.send();
+}
